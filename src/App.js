43d084e6051bf067a905857a7aec6afcfaa306fc
Abhishek Pati <abhishekpati250@gmail.com>
2023-09-02 12:33:32 +0530

    admin-ui first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TableData from "./UI-Components/tableOfUsersData";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <TableData />
+      </header>
+    </div>
+  );
+}
+
+export default App;
